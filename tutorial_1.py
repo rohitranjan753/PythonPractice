de029da3053a392d8a06fb00ea62e9062291e41c
Rohit Ranjan <rohitgns54321@gmail.com>
2023-04-01 15:34:24 +0530

    age

--- tutorial_1.py
+++ tutorial_1.py
@@ -3,3 +3,8 @@ x=int(input("Enter the first number"))
 y=int(input("Enter the second number"))
 a=x+y
 print(a)
+
+
+age=int(input("Enter the age you want to display"))
+name=input("Enter the name")
+message="Hello Mr. "+name+" Your age is "+age
